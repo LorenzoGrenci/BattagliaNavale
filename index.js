84309b2a6b11ab19ccf85d57dfa5effd2df5f424
LorenzoGrenci <grencilorenzo@itis-molinari.eu>
2024-04-28 21:17:42 +0200

    new file:   ca.pem 	new file:   conf.js 	new file:   index.js 	new file:   public/login.html 	new file:   public/login.js 	new file:   public/pagina_principale.html

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const app = express();
+const mysql = require("mysql2");
+const conf = require("./conf.js");
+const connection = mysql.createConnection(conf);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(
+  "/",
+  express.static(path.join(__dirname, "public.pagina_principale.html")),
+);
+
+/*const checkLogin = (user, pass) => {
+  const template = `
+  SELECT * FROM Admin
+  WHERE username = '%username' 
+  AND password = '%password'
+  `;
+  const sql = template.replace("%username", user).replace("%password", pass);
+  console.log(sql);
+  return executeQuery(sql);
+};
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username, password);
+  checkLogin(username, password).then((result) => {
+    console.log(result);
+    if (result.length > 0) {
+      res.json({ result: "Ok" });
+    } else {
+      //login non valida
+      res.status(401); //errore 401
+      res.json({ result: "Unauthorized" });
+    }
+  });
+});
+*/
+
+const server = http.createServer(app);
+server.listen(80, () => {
+  console.log("Server running");
+});
