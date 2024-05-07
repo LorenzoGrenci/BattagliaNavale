b4b448e3ebe5c9422abb61d035cfefe6a273f2c9
LorenzoGrenci <grencilorenzo@itis-molinari.eu>
2024-05-07 11:36:13 +0200

    aggiornamenti pagina partita

---
+++ partita/partita.js
@@ -0,0 +1,29 @@
+let canvas1 = document.getElementById("canvas1");
+let ctx1 = canvas1.getContext("2d");
+let canvas2 = document.getElementById("canvas2");
+let ctx2 = canvas2.getContext("2d");
+let bw = 600;
+let bh = 600;
+
+const primaGriglia=()=>{
+    ctx1.lineWidth = 5;
+    ctx1.strokeStyle = "black";
+    for (let x = 0; x < bw; x += 60) {
+        for (let y = 0; y < bh; y += 60) {
+            ctx1.strokeRect(x + 10, y + 10, 60, 60);
+        }
+    }
+}
+
+const secondaGriglia=()=>{
+    ctx2.lineWidth = 5;
+    ctx2.strokeStyle = "black";
+    for (let x = 0; x < bw; x += 60) {
+        for (let y = 0; y < bh; y += 60) {
+            ctx2.strokeRect(x + 10, y + 10, 60, 60);
+        }
+    }
+}
+
+primaGriglia();
+secondaGriglia();
