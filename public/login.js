ac84a111609ffbf953a4089a49486512ca3282b9
LucasMartire <martirelucas@itis-molinari.eu>
2024-05-13 11:57:31 +0200

    poopoo

--- public/login.js
+++ public/login.js
@@ -4,39 +4,40 @@ let div_privato = document.getElementById("stato_accesso");
 
 
 const checkLog = async (username, password) => {
-    console.log(username, password);
-    try {
-      const funz = await fetch("/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      });
-      const result = await funz.json();
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  console.log(username, password);
+  try {
+    const funz = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const result = await funz.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
   
 button_login.onclick = () => {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    checkLog(username, password).then((result) => {
-      console.log(result);
-      if (result.result == "Ok") {
-        div_login.classList.remove("d-flex");
-        div_login.classList.add("d-none");
-        div_privato.classList.remove("d-none");
-        div_privato.classList.add("d-flex");
-        load().then((js) => {
-          render(js);
-        });
-      }
-    });
-  };
+  let username = document.getElementById("inUsername").value;
+  let password = document.getElementById("inPassword").value;
+  checkLog(username, password).then((result) => {
+    console.log(result);
+    if (result.result == "Ok") {
+      window.location.replace("homepage.html"),
+        //div_login.classList.remove("d-flex");
+        //div_login.classList.add("d-none");
+        //div_privato.classList.remove("d-none");
+        //div_privato.classList.add("d-flex");
+      load().then((js) => {
+        render(js);
+      });
+    }
+  });
+};
   
