ee9f4799c260996cf198f92193e2b32bb055d96c
LucasMartire <martirelucas@itis-molinari.eu>
2024-05-16 08:26:42 +0200

    modifiche

--- public/homepage.js
+++ public/homepage.js
@@ -1,6 +1,6 @@
 const socket=io();
 const template=`<tr>
     <th>$posizione</th>
-    <td><button type="button" id="btn_partita">$username</button></td>
+    <td><button type="button" id="btn_partita"><a href="partita.html">$username</a></button></td>
     <td>$stato</td>
 </tr>`;
