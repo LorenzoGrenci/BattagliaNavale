70f068b6ad7823b539d42160f826b4060c4af322
LorenzoGrenci <grencilorenzo@itis-molinari.eu>
2024-05-21 13:11:08 +0200

    aggiornamento socket

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const mysql = require("mysql2");
 const conf = require("./conf.js");
 const connection = mysql.createConnection(conf);
 const bodyParser = require("body-parser");
-const socketio = require("socket.io");
 
 
 //server
@@ -42,6 +41,50 @@ const executeQuery = (sql) => {
 };
 
 //websocket
+const { Server } = require("socket.io");
+const io = new Server(server);
+let chats = [];
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // Unisciti a una room specifica
+  socket.on("join room", (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+    // Se la chat non esiste ancora, la creiamo
+    if (!chats.find((chat) => chat.chat === room)) {
+      chats.push({ chat: room, messaggi: [] });
+    }
+  });
+
+  // Ascolta i messaggi di chat e li trasmette a tutti nella stessa room
+  socket.on("chat message", (room, { username, message, timestamp }) => {
+    io.to(room).emit("chat message", { username, message, timestamp }); // Trasmetti l'username e il messaggio
+    let chat = chats.find((chat) => chat.chat === room);
+    if (chat) {
+      chat.messaggi.push({
+        autore: username,
+        ora: timestamp,
+        messaggio: message,
+      });
+    }
+
+    console.log(chats);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (data) => {});
+});
+
+
+
+
+
+/*
 const io = socketio(server);
 
 const connessioni = [null, null]
@@ -65,7 +108,7 @@ io.on('connection', socket =>{
   connessioni[indiceGiocatori] = false;
   //dire che giocatore si è connesso
   socket.broadcast.emit('giocatore-connesso', indiceGiocatori);
-});
+});*/
 
 //Login
 const checkLogin = (user, pass) => {
