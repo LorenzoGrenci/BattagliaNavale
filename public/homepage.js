0de187f9e7a02100c48a2c4a34e6de26ab790df0
LucasMartire <martirelucas@itis-molinari.eu>
2024-05-27 17:28:50 +0200

    div con i turni, sfondo canva e ulteriori agg fatto

--- public/homepage.js
+++ public/homepage.js
@@ -1,10 +1,14 @@
 //const div_homepage = document.getElementById("tabella_giocatori");
 const cercaPartita = document.getElementById("btn_partita");
-const jannikSpinner = document.getElementById("div_spinner")
+const jannikSpinner = document.getElementById("div_spinner");
 const paginaPartita = document.getElementById("pagina_partita");
-const modalità = document.getElementById("btn_modalita")
+const modalità = document.getElementById("btn_modalita");
+const div_timer = document.getElementById("timer");
+const div_turno = document.getElementById("turno");
 import {primaGriglia, secondaGriglia, caricaRisultato, visualizzaColpo} from "./partita/partita.js"
  
+const template =`<h2><strong>E'IL TUO TURNO!</strong></h2>`;
+
 let canvas1 = document.getElementById("canvas1");
 let ctx1 = canvas1.getContext("2d");
 let canvas2 = document.getElementById("canvas2");
@@ -32,6 +36,7 @@ const controllaColpo = (ciccio) =>{
             console.log("Cliccato sulla cella: ", xx, yy);
             socket.emit("colpo", {x: xx , y: yy})
             ciccio = false
+            
         }
     });
 }
@@ -57,11 +62,23 @@ socket.on("partita già in corso", ()=>{
 })
 
 socket.on("start turn", ()=>{
-    let ciccio = true
-    alert("è il tuo turno")
-    console.log("è il tuo turno")
-    controllaColpo(ciccio)
-})
+    let ciccio = true;
+    //alert("è il tuo turno");
+    div_turno.innerHTML=`<h2><strong>E'IL TUO TURNO!</strong></h2>`;
+    console.log("è il tuo turno");
+    controllaColpo(ciccio);
+    let time=0;
+    const maxTime=20;
+    const timer = setInterval(()=>{
+        if(time < maxTime){
+            time += 1;
+            div_timer.innerHTML = `<h2><strong>${time} SEC</strong></h2>`;
+        }else{
+            clearInterval(timer);
+            div_timer.innerHTML = ``;
+        };
+    }, 1000);// 1 secondo
+});
 
 socket.on("hit", (data)=>{
     if (data.num===1){
@@ -74,9 +91,13 @@ socket.on("risultato", (data)=>{
     if (data.num===1){
         caricaRisultato(ctx2, data.x, data.y, true)
         console.log("hai colpito la navicella")
+        div_turno.innerHTML=``;
+        div_turno.innerHTML=`<h2><strong>TURNO DELL'AVVERSARIO!</strong></h2>`;
     }else{
         caricaRisultato(ctx2, data.x, data.y, false)
         console.log("non hai colpito niente")
+        div_turno.innerHTML="";
+        div_turno.innerHTML=`<h2><strong>TURNO DELL'AVVERSARIO!</strong></h2>`;
     }
 })
 
